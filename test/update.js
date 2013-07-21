ab90de1d9fc0cf58a33173ed06eefc2e8e3fbe1f
ajlopez <webmaster@ajlopez.com>
2013-07-21 06:36:31 -0300

    Update first test

---
+++ test/update.js
@@ -0,0 +1,17 @@
+
+var ostore = require('..'),
+    assert = require('assert');
+    
+// update age
+
+var store = ostore.createStore();
+var adam = { name: 'Adam', age: 800 };
+store.put(1, adam);
+
+store.update(1, { age: 801 });
+
+var result = store.get(1);
+assert.ok(result);
+assert.equal(result.name, 'Adam');
+assert.equal(result.age, 801);
+
