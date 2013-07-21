e624ad6ecc1085e18e7f86895a6294c6968ea88d
ajlopez <webmaster@ajlopez.com>
2013-07-21 06:30:54 -0300

    Additional test, get and put two items

--- test/getput.js
+++ test/getput.js
@@ -19,3 +19,19 @@ assert.ok(result);
 assert.equal(result.name, 'Adam');
 assert.equal(result.age, 800);
 
+// put and get two values
+
+var store = ostore.createStore();
+var adam = { name: 'Adam', age: 800 };
+store.put(1, adam);
+var eve = { name: 'Eve', age: 700 };
+store.put(2, eve);
+var result = store.get(1);
+assert.ok(result);
+assert.equal(result.name, 'Adam');
+assert.equal(result.age, 800);
+var result = store.get(2);
+assert.ok(result);
+assert.equal(result.name, 'Eve');
+assert.equal(result.age, 700);
+
