74f03dc9c7ed9d1bbe0a5d626e0fe05f30775561
ajlopez <webmaster@ajlopez.com>
2015-04-29 15:17:23 -0300

    operator $ne first test

--- test/find.js
+++ test/find.js
@@ -60,6 +60,19 @@ exports['find using $eq operator'] = function (test) {
     test.equal(result.length, 0);
 };
 
+exports['find using $eq operator'] = function (test) {
+    var result = store.find({ age: { $ne: 700 } });
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 1);
+    test.equal(result[0].age, 800);
+
+    var result = store.find({ age: { $ne: 800 } });
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 0);
+};
+
 exports['find using two properties at criteria, failing'] = function (test) {
     var result = store.find({ name: 'Adam', age: 600 });
     test.ok(result);
