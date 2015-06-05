204eab17f055c03955a56c634f66ae0d54c4ddf4
ajlopez <webmaster@ajlopez.com>
2015-06-05 06:17:04 -0300

    Operator $multiply on many numbers

--- test/aggregate.js
+++ test/aggregate.js
@@ -477,6 +477,28 @@ exports['aggregate with project and new field with simple expression using multi
     test.ok(result[2].id);
 };
 
+exports['aggregate with project and new field with simple expression using multiply many numbers'] = function (test) {
+    var result = store.aggregate({ $project: { factorial: { $multiply: [ 1, 2, 3, 4, 5 ] }}});
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 3);
+    
+    test.equal(result[0].name, 'Adam');
+    test.equal(result[0].age, 800);
+    test.strictEqual(result[0].factorial, 120);
+    test.ok(result[0].id);
+    
+    test.equal(result[1].name, 'Eve');
+    test.equal(result[1].age, 700);
+    test.strictEqual(result[1].factorial, 120);
+    test.ok(result[1].id);
+    
+    test.equal(result[2].name, 'Abel');
+    test.equal(result[2].age, 600);
+    test.strictEqual(result[2].factorial, 120);
+    test.ok(result[2].id);
+};
+
 exports['aggregate with project excluding id field'] = function (test) {
     var result = store.aggregate({ $project: { id: false } });
     test.ok(result);
