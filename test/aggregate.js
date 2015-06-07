09dc4fa3ee1e2b78ff3b98257ba40273aa9833cd
ajlopez <webmaster@ajlopez.com>
2015-06-07 09:19:38 -0300

    Operator $mod in projection aggregate

--- test/aggregate.js
+++ test/aggregate.js
@@ -323,6 +323,28 @@ exports['aggregate with project and new field with compare'] = function (test) {
     test.ok(result[2].id);
 };
 
+exports['aggregate with project and new field with mod'] = function (test) {
+    var result = store.aggregate({ $project: { modulus: { $mod: [ "$age", 7 ] }}});
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 3);
+    
+    test.equal(result[0].name, 'Adam');
+    test.equal(result[0].age, 800);
+    test.strictEqual(result[0].modulus, 800 % 7);
+    test.ok(result[0].id);
+    
+    test.equal(result[1].name, 'Eve');
+    test.equal(result[1].age, 700);
+    test.strictEqual(result[1].modulus, 700 % 7);
+    test.ok(result[1].id);
+    
+    test.equal(result[2].name, 'Abel');
+    test.equal(result[2].age, 600);
+    test.strictEqual(result[2].modulus, 600 % 7);
+    test.ok(result[2].id);
+};
+
 exports['aggregate with project and new field with simple expressions greater than or equal'] = function (test) {
     var result = store.aggregate({ $project: { greaterThanOrEqual700: { $gte: [ "$age", 700 ] }}});
     test.ok(result);
