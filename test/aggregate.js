22515ab82fa4381a882ab481dd8bbf6a82eae1eb
ajlopez <webmaster@ajlopez.com>
2015-06-10 07:03:20 -0300

    Operator $toLower

--- test/aggregate.js
+++ test/aggregate.js
@@ -433,6 +433,28 @@ exports['aggregate with project and new field with simple expressions concat man
     test.ok(result[2].id);
 };
 
+exports['aggregate with project and new field with simple expression toLower'] = function (test) {
+    var result = store.aggregate({ $project: { lowerName: { $toLower: "$name" }}});
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 3);
+    
+    test.equal(result[0].name, 'Adam');
+    test.equal(result[0].age, 800);
+    test.strictEqual(result[0].lowerName, 'adam');
+    test.ok(result[0].id);
+    
+    test.equal(result[1].name, 'Eve');
+    test.equal(result[1].age, 700);
+    test.strictEqual(result[1].lowerName, 'eve');
+    test.ok(result[1].id);
+    
+    test.equal(result[2].name, 'Abel');
+    test.equal(result[2].age, 600);
+    test.strictEqual(result[2].lowerName, 'abel');
+    test.ok(result[2].id);
+};
+
 exports['aggregate with project and new field with simple expressions add and number'] = function (test) {
     var result = store.aggregate({ $project: { nextAge: { $add: [ "$age", 1 ] }}});
     test.ok(result);
