0cae80eb61add5e00b4de14a06d4abd66e9a8e99
ajlopez <webmaster@ajlopez.com>
2015-09-05 17:36:55 -0300

    Create a store with name

--- test/create.js
+++ test/create.js
@@ -5,3 +5,9 @@ exports['create a store'] = function (test) {
     var store = ostore.createStore();
     test.ok(store);
 }
+
+exports['create a store with name'] = function (test) {
+    var store = ostore.createStore('persons');
+    test.ok(store);
+    test.equal(store.name(), 'persons');
+}
