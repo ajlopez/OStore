56c2627643ee4ed2895fb89a3725154872372cb0
ajlopez <webmaster@ajlopez.com>
2015-12-13 08:51:01 -0300

    Remove item using criteria

--- test/remove.js
+++ test/remove.js
@@ -22,3 +22,21 @@ exports['add and remove an item'] = function (test) {
     
     test.equal(store.get(result), null);
 };
+
+exports['remove item using criteria'] = function (test) {
+    var store = ostore.createStore();
+    var adam = { name: 'Adam', age: 800 };
+    var eve = { name: 'Eve', age: 700 };
+    
+    store.add(adam);
+    store.add(eve);
+    
+    store.remove({ name: 'Adam' });
+    
+    var result = store.find();
+    
+    test.ok(result);
+    test.equal(result.length, 1);
+    test.equal(result[0].name, 'Eve');
+    test.equal(result[0].age, 700);
+};
