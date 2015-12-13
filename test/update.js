90fc63ba34f9b252e03353e472c491ebb06b997b
ajlopez <webmaster@ajlopez.com>
2015-12-13 09:14:50 -0300

    Update age using criteria

--- test/update.js
+++ test/update.js
@@ -13,3 +13,26 @@ exports['update age'] = function (test) {
     test.equal(result.name, 'Adam');
     test.equal(result.age, 801);
 }
+
+exports['update age using criteria'] = function (test) {
+    var store = ostore.createStore();
+    var adam = { name: 'Adam', age: 800 };
+    var eve = { name: 'Eve', age: 700 };
+    
+    store.add(adam);
+    store.add(eve);
+
+    store.update({ name: 'Adam' }, { age: 801 });
+
+    var result = store.find({ name: 'Adam' });
+    test.ok(result);
+    test.equal(result.length, 1);
+    test.equal(result[0].name, 'Adam');
+    test.equal(result[0].age, 801);
+
+    var result = store.find({ name: 'Eve' });
+    test.ok(result);
+    test.equal(result.length, 1);
+    test.equal(result[0].name, 'Eve');
+    test.equal(result[0].age, 700);
+}
